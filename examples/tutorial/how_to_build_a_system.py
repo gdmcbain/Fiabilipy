2da71607b0851a99111db9d3c52e0f65a62023bb
G. D. McBain <gdmcbain@pm.me>
2024-05-30 12:04:21 +1000

    Draw graphics

--- examples/tutorial/how_to_build_a_system.py
+++ examples/tutorial/how_to_build_a_system.py
@@ -1,5 +1,9 @@
-from fiabilipy import Component, System
+from pathlib import Path
+
+from matplotlib.pyplot import subplots
 from sympy import Symbol
+
+from fiabilipy import Component, System
 
 # Building components
 
@@ -25,3 +29,30 @@ print("\n# Gather components to build a system")
 print(f"{S.mttf=}")
 print(f"{float(S.mttf)=}")
 print(f"{S.reliability(t)=}")
+
+# An example of a complex system
+
+a, b, c, d, e, f, g = [Component(f"C{i}", 1e-4) for i in range(7)]
+S = System()
+S["E"] = [a, b, c, g]
+S[a] = S[g] = S[e] = S[d] = "S"
+S[b] = S[c] = [f]
+S[f] = [e, d]
+
+print("\n# An example of a complex system")
+print(f"{S.mttf=}")
+print(f"{S.reliability(t)=}")
+
+# Draw graphics
+
+a, b = Component("a", 1e-4), Component("b", 1e-6)
+S = System()
+S["E"] = [a, b]
+S[a] = S[b] = "S"
+
+timerange = range(0, 20000, 100)
+reliability = [S.reliability(t) for t in timerange]
+
+fig, ax = subplots()
+ax.plot(timerange, reliability)
+fig.savefig(Path(__file__).with_suffix(".png"))
